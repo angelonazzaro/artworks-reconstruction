fff8b694b4bff2282678b7b95a7b36036c9b7c88
raffaele-aurucci <raffaeleaurucci.informatica@gmail.com>
2024-05-01 16:21:25 +0200

    add implementation for extracting interpolation region
    Co-authored-by: Angelo Nazzaro <angelonazzaro12@gmail.com>
    Co-authored-by: Angelo Palmieri <palmieri309@gmail.com>

--- utils/fourier.py
+++ utils/fourier.py
@@ -71,3 +71,39 @@ def alpha_edge_detector(input_img):
                     edges[x][y] = 1
                     break
     return edges
+
+
+def extract_edge_interpolation_region(input_img, threshold=5):
+    rows, cols, channels = input_img.shape
+    interpolation_region = np.zeros((rows, cols, channels), dtype=np.uint8)
+
+    neighbor_offsets = np.array([[1, 0], [-1, 0], [0, 1], [0, -1]])
+
+    for x in range(rows):
+        for y in range(cols):
+            for offset_x, offset_y in neighbor_offsets:
+                neighbor_x = x + offset_x
+                neighbor_y = y + offset_y
+
+                # Check if neighbor is out of bounds or has alpha zero and move in the opposite direction
+                if (not (0 <= neighbor_x < rows and 0 <= neighbor_y < cols)
+                        or input_img[neighbor_x][neighbor_y][3] == 0):
+
+                    # Add all the valid pixel in [1, threshold] to the interpolation region
+                    for t in range(1, threshold + 1):
+                        if offset_x > 0 or offset_y > 0:
+                            opposite_x = neighbor_x - t if offset_x != 0 else neighbor_x
+                            opposite_y = neighbor_y - t if offset_y != 0 else neighbor_y
+                        else:
+                            opposite_x = neighbor_x + t if offset_x != 0 else neighbor_x
+                            opposite_y = neighbor_y + t if offset_y != 0 else neighbor_y
+
+                        # check if pixel is out of bounds
+                        if not (0 <= opposite_x < rows and 0 <= opposite_y < cols):
+                            break
+
+                        # if inner pixel has content, add it to the interpolation region
+                        if input_img[opposite_x][opposite_y][3] != 0:
+                            interpolation_region[opposite_x][opposite_y] = input_img[opposite_x][opposite_y]
+
+    return interpolation_region

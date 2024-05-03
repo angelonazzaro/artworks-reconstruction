150c9956909fe681b0166b92e5675ed7b88c34cf
Angelo Nazzaro <mutenssj@gmail.com>
2024-05-03 19:49:06 +0200

    add angular histogram computation
    Co-authored-by: raffaele-aurucci <raffaeleaurucci.informatica@gmail.com>
    Co-authored-by: AngeloPalmieri <a.palmieri309@gmail.com>

--- utils/fourier.py
+++ utils/fourier.py
@@ -2,6 +2,40 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def calculate_dft(input_signal: np.ndarray) -> np.ndarray:
+    """
+    Calculates the 1D Discrete Fourier Transform (DFT) of a 1D signal.
+
+    Args:
+    - input_img (np.ndarray): Input signal as a 1D numpy array.
+
+    Returns:
+    - ft (np.ndarray): 1D DFT of the input signal, Fourier coefficients shifted to the center.
+
+    This function computes the 2D DFT of the input image using numpy's fft.fft() function.
+    The resulting DFT coefficients are shifted to center the low frequencies.
+    """
+    ft = np.fft.fft(input_signal)
+    return np.fft.fftshift(ft)
+
+
+def calculate_idft(input_img: np.ndarray) -> np.ndarray:
+    """
+    Calculates the 1D Inverse Discrete Fourier Transform (IDFT) of a 1D signal.
+
+    Args:
+    - input_img (np.ndarray): Input signal as a 1D numpy array.
+
+    Returns:
+    - ifft (np.ndarray): 1D IDFT of the input signal, real part of the inverse transform.
+
+    This function computes the 1D IDFT of the input image using numpy's fft.ifft() function.
+    The input signal is expected to have been shifted to center the low frequencies.
+    """
+    ifft = np.fft.ifftshift(input_img)
+    return np.fft.ifft(ifft).real
+
+
 def calculate_dft2(input_img: np.ndarray) -> np.ndarray:
     """
     Calculates the 2D Discrete Fourier Transform (DFT) of an image.
@@ -36,7 +70,24 @@ def calculate_idft2(input_img: np.ndarray) -> np.ndarray:
     return np.fft.ifft2(ifft).real
 
 
-def plot_ft_spectrum(ft: np.ndarray):
+def plot_ft_spectrum(ft: np.ndarray, xlabel: str = None, ylabel: str = None, title: str = None) -> None:
+    """
+    Plots the spectrum of the 1D Fourier Transform.
+
+    Args:
+    - ft (np.ndarray): 1D Fourier Transform coefficients.
+
+    This function plots the magnitude spectrum of the 1D Fourier Transform.
+    The Fourier coefficients are expected to be shifted to center the low frequencies.
+    """
+    plt.plot(np.log(abs(ft)))
+    plt.xlabel(xlabel=xlabel)
+    plt.ylabel(ylabel=ylabel)
+    plt.title(title=title)
+    plt.show()
+
+
+def plot_2d_ft_spectrum(ft: np.ndarray, xlabel: str = None, ylabel: str = None, title: str = None) -> None:
     """
     Plots the spectrum of the 2D Fourier Transform.
 
@@ -48,10 +99,14 @@ def plot_ft_spectrum(ft: np.ndarray):
     """
     plt.set_cmap("gray")
     plt.imshow(np.log(abs(ft)))
+    plt.xlabel(xlabel=xlabel)
+    plt.ylabel(ylabel=ylabel)
+    plt.title(title=title)
     plt.axis("off")
+    plt.show()
 
 
-def apply_low_pass_filter(ft: np.ndarray, threshold: int) -> np.ndarray:
+def apply_2d_low_pass_filter(ft: np.ndarray, threshold: int) -> np.ndarray:
     """
     Applies a low-pass filter to the Fourier Transform.
 
@@ -72,7 +127,7 @@ def apply_low_pass_filter(ft: np.ndarray, threshold: int) -> np.ndarray:
     return ft * mask
 
 
-def apply_high_pass_filter(ft: np.ndarray, threshold: int) -> np.ndarray:
+def apply_2d_high_pass_filter(ft: np.ndarray, threshold: int) -> np.ndarray:
     """
     Applies a high-pass filter to the Fourier Transform.
 
@@ -93,7 +148,7 @@ def apply_high_pass_filter(ft: np.ndarray, threshold: int) -> np.ndarray:
     return ft * mask
 
 
-def apply_band_pass_filter(ft: np.ndarray, lower_threshold: int, upper_threshold: int) -> np.ndarray:
+def apply_2d_band_pass_filter(ft: np.ndarray, lower_threshold: int, upper_threshold: int) -> np.ndarray:
     """
     Applies a band-pass filter to the Fourier Transform.
 
@@ -110,6 +165,6 @@ def apply_band_pass_filter(ft: np.ndarray, lower_threshold: int, upper_threshold
     """
     assert lower_threshold < upper_threshold
     w = (upper_threshold - lower_threshold) // 2
-    ft = apply_high_pass_filter(ft, lower_threshold - w)
-    ft = apply_low_pass_filter(ft, upper_threshold + w)
+    ft = apply_2d_high_pass_filter(ft, lower_threshold - w)
+    ft = apply_2d_low_pass_filter(ft, upper_threshold + w)
     return ft
